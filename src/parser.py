91274c6eaa5cb46698462143bdd73b0241c16f28
Alex Mayer <mayeralex05@gmail.com>
2025-11-18 19:56:12 -0600

    Made Parser.py to hanlt .txt files

--- src/parser.py
+++ src/parser.py
@@ -1,15 +1,17 @@
 # cleans up files for extraction of key details later
 
 def parse_resume_txt(path: str) -> str:
-    file = open(path)
-    data = file.read()
-    file.close()
-    return data
+    result = ""
+    with open(path) as file:
+        for line in file:
+            result += line.strip()
+    return result
 
 def parse_job_txt(path: str) -> str:
-    file = open(path)
-    data = file.read()
-    file.close()
-    return data
+    result = ""
+    with open(path) as file:
+        for line in file:
+            result += line.strip()
+    return result
 
 
